ab5f808aee46e19779e433524080b875564f18af
wangbo <bbw1996@qq.com>
2021-05-06 22:28:20 +0800

    搭建html结构

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="topNav">
+      <div class="logo"></div>
+      <div class="menu"></div>
+    </div>
+    <div class="banner">
+      <h1>Vio UI</h1>
+      <h2>一款不错的 UI 框架</h2>
+      <p class="actions">
+        <a href="">Github</a>
+        <a href="">开始</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+
+</style>
